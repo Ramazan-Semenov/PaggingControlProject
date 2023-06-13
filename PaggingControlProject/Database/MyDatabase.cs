5d1393b8b9e8d7fef5f0196cb6c9000340f8a674
semenov ramazan <rombik.s@inbox.ru>
2023-06-13 16:46:08 +0300

    Добавьте файлы проекта.

---
+++ PaggingControlProject/Database/MyDatabase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PaggingControlProject.Database
+{
+    public class MyDatabase : IPageControlContract
+    {
+        List<Student> studentList = new List<Student>();
+
+        public MyDatabase()
+        {
+            Student studentObj;
+            Random randomObj = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                studentObj = new Student();
+                studentObj.FirstName = "First " + i;
+                studentObj.MiddleName = "Middle " + i;
+                studentObj.LastName = "Last " + i;
+                studentObj.Age = (uint)randomObj.Next(1, 100);
+
+                studentList.Add(studentObj);
+            }
+        }
+
+        #region IPageControlContract Members
+
+        public uint GetTotalCount()
+        {
+            return (uint)studentList.Count;
+        }
+
+        public ICollection<object> GetRecordsBy(uint StartingIndex, uint NumberOfRecords, object FilterTag)
+        {
+            if (StartingIndex >= studentList.Count)
+            {
+                return new List<object>();
+            }
+
+            List<Student> result = new List<Student>();
+
+            for (int i = (int)StartingIndex; i < studentList.Count && i < StartingIndex + NumberOfRecords; i++)
+            {
+                result.Add(studentList[i]);
+            }
+
+            return result.ToList<object>();
+        }
+
+        #endregion
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public uint Age { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + "." + MiddleName + "." + LastName + " (" + Age + ")";
+        }
+    }
+}
---
+++ PaggingControlProject/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaggingControlProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pageControl_PreviewPageChange(object sender, PageChangedEventArgs args)
+        {
+            List<Object> items = pageControl.ItemsSource.ToList();
+            int count = items.Count;
+        }
+
+        private void pageControl_PageChanged(object sender, PageChangedEventArgs args)
+        {
+            List<Object> items = pageControl.ItemsSource.ToList();
+            int count = items.Count;
+        }
+    }
+}
